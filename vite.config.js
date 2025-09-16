6d3cee96401a0bbacf72da47997ec4e71fa895cb
Samir Elanany <samir.elanany@microengineering.ca>
2025-09-16 16:35:00 +0200

    handle not found page path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+function copyRedirects() {
+  return {
+    name: "copy-redirects",
+    closeBundle() {
+      fs.copyFileSync("public/_redirects", "dist/_redirects");
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), copyRedirects()],
+});
